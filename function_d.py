fc08eb4b3eea6aa5996a09effb77d53cb1ecdd2c
Tatiana Didenko <tatiana.a.didenko@gmail.com>
2025-09-22 14:46:21 -0700

    Tatiana

--- function_d.py
+++ function_d.py
@@ -1,4 +1,10 @@
 def max_value(numbers):
+    """ This function returns the largest number in the list. """
+    max_num = numbers[0]   # берём первый элемент как максимум
+    for num in numbers:    # проходим по всем элементам
+        if num > max_num:  # если нашли больше
+            max_num = num
+    return max_num
     """ This function returns the largest number
         in the list.
     """
