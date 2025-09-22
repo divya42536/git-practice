71747e90be56b41def4e8e3725d6884d47246843
divya42536 <divya42536@gmail.com>
2025-09-22 14:58:45 -0700

    added sum to func_d

--- function_d.py
+++ function_d.py
@@ -1,4 +1,11 @@
+def sum_value(numbers):
 def max_value(numbers):
+    """ This function returns the largest number in the list. """
+    max_num = numbers[0]   # берём первый элемент как максимум
+    for num in numbers:    # проходим по всем элементам
+        if num > max_num:  # если нашли больше
+            max_num = num
+    return max_num
     """ This function returns the largest number
         in the list.
     """
